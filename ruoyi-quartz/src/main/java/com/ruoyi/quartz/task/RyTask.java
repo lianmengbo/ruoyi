79367aef1538effedc9151bfebed2ea32e5b8ee6
lian <mblian0926@163.com>
2023-06-28 16:48:57 +0800

    2023-6-28

--- ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/RyTask.java
+++ ruoyi-quartz/src/main/java/com/ruoyi/quartz/task/RyTask.java
@@ -1,7 +1,12 @@
 package com.ruoyi.quartz.task;
 
+import com.ruoyi.framework.websocket.WebSocketServer;
+import com.ruoyi.framework.websocket.WebSocketUsers;
 import org.springframework.stereotype.Component;
 import com.ruoyi.common.utils.StringUtils;
+
+import javax.annotation.Resource;
+import javax.websocket.Session;
 
 /**
  * 定时任务调度测试
@@ -11,6 +16,8 @@ import com.ruoyi.common.utils.StringUtils;
 @Component("ryTask")
 public class RyTask
 {
+    @Resource
+    private WebSocketServer webSocketServer;
     public void ryMultipleParams(String s, Boolean b, Long l, Double d, Integer i)
     {
         System.out.println(StringUtils.format("执行多参方法： 字符串类型{}，布尔类型{}，长整型{}，浮点型{}，整形{}", s, b, l, d, i));
@@ -23,6 +30,6 @@ public class RyTask
 
     public void ryNoParams()
     {
-        System.out.println("执行无参方法");
+
     }
 }
